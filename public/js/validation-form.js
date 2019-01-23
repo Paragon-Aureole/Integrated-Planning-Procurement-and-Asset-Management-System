cf1a936e7d11cc89e239e1c370fb728d424a57e8
reinhard1296 <cdm4c4lin0@gmail.com>
2019-01-23 21:17:26 +0800

    package_installation

---
+++ public/js/validation-form.js
@@ -0,0 +1,19 @@
+(function () {  
+        'use strict';  
+        window.addEventListener('load', function () {  
+            var form = document.getElementById('needs-validation');  
+            form.addEventListener('submit', function (event) {  
+                if (form.checkValidity() === false) {  
+                    event.preventDefault();  
+                    event.stopPropagation();  
+                }  
+                form.classList.add('was-validated');  
+            }, false);  
+        }, false);  
+    })();
+
+$(document).ready(function(){
+    setTimeout(function() {
+        $(".alert").alert('close');
+    }, 3000);
+});
